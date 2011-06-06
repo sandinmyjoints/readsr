467a10a286b632cad8137f0bc297d7df1cd19172
William Bert <william.bert@gmail.com>
2011-06-06 08:12:20 -0400

    Cleaned up the code for the calendar a bit

--- reading/templatetags/reading_tags.py
+++ reading/templatetags/reading_tags.py
@@ -1,25 +1,31 @@
 from calendar import HTMLCalendar
 from django import template
-from reading.reading_calendar import ReadingCalendar
+from datetime import date
+from itertools import groupby
+
+from django.utils.html import conditional_escape as esc
 
 register = template.Library()
 
 def do_reading_calendar(parser, token):
+	"""
+	The template tag's syntax is {% reading_calendar year month reading_list %}
+    """
+
 	try:
-        # syntax is {% reading_calendar year month reading_list %}
-#		tag_name, year, month = token.split_contents()
 		tag_name, year, month, reading_list = token.split_contents()
 	except ValueError:
 		raise template.TemplateSyntaxError, "%r tag requires three arguments" % token.contents.split()[0]
-#	return ReadingCalendarNode(year, month)
 	return ReadingCalendarNode(year, month, reading_list)
 	
 
 class ReadingCalendarNode(template.Node):
-#	def __init__(self, year, month):
+	"""
+	Process a particular node in the template. Fail silently.
+	"""
+	
 	def __init__(self, year, month, reading_list):
 		try:
-			#print "incoming year is %s, incoming month is %s" % (year, month)
 			self.year = template.Variable(year)
 			self.month = template.Variable(month)
 			self.reading_list = template.Variable(reading_list)
@@ -28,19 +34,58 @@ class ReadingCalendarNode(template.Node):
 		
 	def render(self, context):
 		try:
+			# Get the variables from the context so the method is thread-safe.
 			my_reading_list = self.reading_list.resolve(context)
 			my_year = self.year.resolve(context)
 			my_month = self.month.resolve(context)
-			print "args are %d %d %d" % (int(my_year), int(my_month), True)
 			cal = ReadingCalendar(my_reading_list)
-			#cal = HTMLCalendar()
-			response = cal.formatmonth(int(my_year), int(my_month))
-			print "response is %s" % response
-			return response
+			return cal.formatmonth(int(my_year), int(my_month))
 		except ValueError:
 			return			
 		except template.VariableDoesNotExist:
 			return
-		
+
+
+class ReadingCalendar(HTMLCalendar):
+	"""
+	Overload Python's calendar.HTMLCalendar to add the appropriate reading events to
+	each day's table cell.
+	"""
+	
+	def __init__(self, readings):
+		super(ReadingCalendar, self).__init__()
+		self.readings = self.group_by_day(readings)
+
+	def formatday(self, day, weekday):
+		if day != 0:
+			cssclass = self.cssclasses[weekday]
+			if date.today() == date(self.year, self.month, day):
+				cssclass += ' today'
+			if day in self.readings:
+				cssclass += ' filled'
+				body = ['<ul>']
+				for reading in self.readings[day]:
+					body.append('<li>')
+					body.append('<a href="%s">' % reading.get_absolute_url())
+					body.append(esc(reading.series.primary_name))
+					body.append('</a></li>')
+				body.append('</ul>')
+				return self.day_cell(cssclass, '<span class="dayNumber">%d</span> %s' % (day, ''.join(body)))
+			return self.day_cell(cssclass, '<span class="dayNumberNoReadings">%d</span>' % (day))
+		return self.day_cell('noday', '&nbsp;')
+
+	def formatmonth(self, year, month):
+		self.year, self.month = year, month
+		return super(ReadingCalendar, self).formatmonth(year, month)
+
+	def group_by_day(self, readings):
+		field = lambda reading: reading.date_and_time.day
+		return dict(
+			[(day, list(items)) for day, items in groupby(readings, field)]
+		)
+
+	def day_cell(self, cssclass, body):
+		return '<td class="%s">%s</td>' % (cssclass, body)
+
+# Register the template tag so it is available to templates
 register.tag("reading_calendar", do_reading_calendar)
-		
