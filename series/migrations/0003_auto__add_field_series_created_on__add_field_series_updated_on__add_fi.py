12c289cfd69a08f09562e5d828f417f59c076a84
William Bert <william.bert@gmail.com>
2011-06-26 21:24:15 -0400

    Forgot to add the new files (migrations) so adding them this time.

---
+++ series/migrations/0003_auto__add_field_series_created_on__add_field_series_updated_on__add_fi.py
@@ -0,0 +1,256 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Series.created_on'
+        db.add_column('series_series', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'Series.updated_on'
+        db.add_column('series_series', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'DayOfWeek.created_on'
+        db.add_column('series_dayofweek', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'DayOfWeek.updated_on'
+        db.add_column('series_dayofweek', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'Contact.created_on'
+        db.add_column('series_contact', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'Contact.updated_on'
+        db.add_column('series_contact', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'Genre.created_on'
+        db.add_column('series_genre', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'Genre.updated_on'
+        db.add_column('series_genre', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'Address.created_on'
+        db.add_column('series_address', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'Address.updated_on'
+        db.add_column('series_address', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'WeekWithinMonth.created_on'
+        db.add_column('series_weekwithinmonth', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'WeekWithinMonth.updated_on'
+        db.add_column('series_weekwithinmonth', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'Affiliate.created_on'
+        db.add_column('series_affiliate', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'Affiliate.updated_on'
+        db.add_column('series_affiliate', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+        # Adding field 'SeriesTweet.created_on'
+        db.add_column('series_seriestweet', 'created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True), keep_default=False)
+
+        # Adding field 'SeriesTweet.updated_on'
+        db.add_column('series_seriestweet', 'updated_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now=True, blank=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Series.created_on'
+        db.delete_column('series_series', 'created_on')
+
+        # Deleting field 'Series.updated_on'
+        db.delete_column('series_series', 'updated_on')
+
+        # Deleting field 'DayOfWeek.created_on'
+        db.delete_column('series_dayofweek', 'created_on')
+
+        # Deleting field 'DayOfWeek.updated_on'
+        db.delete_column('series_dayofweek', 'updated_on')
+
+        # Deleting field 'Contact.created_on'
+        db.delete_column('series_contact', 'created_on')
+
+        # Deleting field 'Contact.updated_on'
+        db.delete_column('series_contact', 'updated_on')
+
+        # Deleting field 'Genre.created_on'
+        db.delete_column('series_genre', 'created_on')
+
+        # Deleting field 'Genre.updated_on'
+        db.delete_column('series_genre', 'updated_on')
+
+        # Deleting field 'Address.created_on'
+        db.delete_column('series_address', 'created_on')
+
+        # Deleting field 'Address.updated_on'
+        db.delete_column('series_address', 'updated_on')
+
+        # Deleting field 'WeekWithinMonth.created_on'
+        db.delete_column('series_weekwithinmonth', 'created_on')
+
+        # Deleting field 'WeekWithinMonth.updated_on'
+        db.delete_column('series_weekwithinmonth', 'updated_on')
+
+        # Deleting field 'Affiliate.created_on'
+        db.delete_column('series_affiliate', 'created_on')
+
+        # Deleting field 'Affiliate.updated_on'
+        db.delete_column('series_affiliate', 'updated_on')
+
+        # Deleting field 'SeriesTweet.created_on'
+        db.delete_column('series_seriestweet', 'created_on')
+
+        # Deleting field 'SeriesTweet.updated_on'
+        db.delete_column('series_seriestweet', 'updated_on')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'city_site.citysite': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'CitySite', '_ormbases': ['sites.Site']},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'site_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['sites.Site']", 'unique': 'True', 'primary_key': 'True'}),
+            'state': ('django.contrib.localflavor.us.models.USStateField', [], {'max_length': '2'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'series.address': {
+            'Meta': {'object_name': 'Address'},
+            'city_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'state': ('django.contrib.localflavor.us.models.USStateField', [], {'max_length': '2'}),
+            'street_address': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
+            'zip_code': ('django.db.models.fields.CharField', [], {'max_length': '5'})
+        },
+        'series.affiliate': {
+            'Meta': {'object_name': 'Affiliate'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'series.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'series.dayofweek': {
+            'Meta': {'object_name': 'DayOfWeek'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'day': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'})
+        },
+        'series.genre': {
+            'Meta': {'object_name': 'Genre'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'genre': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'})
+        },
+        'series.series': {
+            'Meta': {'ordering': "('primary_name',)", 'object_name': 'Series'},
+            'admission': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'admission_description': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'affiliations': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['series.Affiliate']", 'null': 'True', 'blank': 'True'}),
+            'contact': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'day_of_week': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['series.DayOfWeek']", 'null': 'True', 'blank': 'True'}),
+            'genre': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['series.Genre']", 'symmetrical': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'irregular_date_description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'last_update': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'notes': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'primary_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
+            'regular': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'secondary_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['city_site.CitySite']"}),
+            'time': ('django.db.models.fields.TimeField', [], {'default': 'datetime.time(18, 0)'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
+            'venue': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['series.Venue']"}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'week_within_month': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['series.WeekWithinMonth']", 'null': 'True', 'blank': 'True'}),
+            'wiki_mode': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'series.seriestweet': {
+            'Meta': {'object_name': 'SeriesTweet'},
+            'bitly_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'series': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['series.Series']", 'null': 'True'}),
+            'tweet': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
+            'twitter_status_id': ('django.db.models.fields.CharField', [], {'max_length': '17'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'})
+        },
+        'series.venue': {
+            'Meta': {'object_name': 'Venue'},
+            'address': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['series.Address']", 'unique': 'True'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_dc': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'notes': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'phone': ('django.contrib.localflavor.us.models.PhoneNumberField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'secondary_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'series.weekwithinmonth': {
+            'Meta': {'object_name': 'WeekWithinMonth'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
+            'week_within_month': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['series']
