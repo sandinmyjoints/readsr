584ce56bfa2edb7d8c43f16d0a364c835fe2cd5c
William Bert <william.bert@gmail.com>
2011-08-13 12:38:58 -0400

    Added int2word and kept working on converting monthly to text. Still failing first unit test.

--- series/rrule2text/rrule2text.py
+++ series/rrule2text/rrule2text.py
@@ -10,10 +10,18 @@ Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 import sys
 import os
 import unittest
+from datetime import datetime
 
 from django.utils.translation import ugettext_lazy as _ # TODO change this to Python
 
-from dateutil import rrule, parser, relativedelta
+from dateutil.rrule import *
+from dateutil.parser import *
+from dateutil.relativedelta import *
+
+from readsr.series.rrule2text.int2word import int2word
+
+class Rrule2textError(ValueError):
+    pass
 
 class rrule2text(rrule):
 
@@ -34,18 +42,76 @@ class rrule2text(rrule):
         (5, _(u'Friday')),
         (6, _(u'Saturday'))
     )
-    		
-	def rrule2text(self):
-        # first verify that this is a valid monthly recurring date
-        rule_pair = self._bynweekday[0]
-        # The first item of rule_pair is the weekday
-        weekday = WEEKDAY_LONG[rule_pair[0] % 7][1]
-        print "weekday is %s" % weekday
+    
+    INTERVAL = (
+        (1, _(u'each')),
+        (2, _(u'every other')),
+        (3, _(u'every third')),
+        (4, _(u'every fourth')),
+        (5, _(u'every fifth')),
+        (6, _(u'every sixth')),
+        (7, _(u'every seventh')),
+        (8, _(u'every eighth')),
+        (9, _(u'every ninth')),
+        (10, _(u'every tenth')),
+        (11, _(u'every eleventh')),
+        (12, _(u'every twelfth')),
+    )
+
+    def rrule2text(self):
+
+        freq = self._freq
+        interval = self._interval
+        wkst = self._wkst
+        until = self._until
+        count = self._count
+        bymonth = self._bymonth
+        byweekno = self._byweekno
+        byyearday = self._byyearday
+        byweekday = self._byweekday
+        bynweekday = self._bynweekday
+        byeaster = self._byeaster
+        bymonthday = self._bymonthday
+        bynmonthday = self._bynmonthday
+        bysetpos = self._bysetpos
+        byhour = self._byhour
+        byminute = self._byminute
+        bysecond = self._bysecond
         
-        # The second item is the ordinal.
-        ordinal = ORDINAL[rule_pair[1]]
-        text = _("%s %s" % (rule_pair[1], rrule.weekdays[rule_pair[0]])
-        return text
+        text_description = []
+        if freq != MONTHLY:
+            raise Rrule2textError, "rrule2text only works with monthly frequencies right now."        
+            
+        # Get the interval. "Each", "Every other", "Every third", etc.
+        p_interval = rrule2text.INTERVAL[interval][1]
+        text_description.append(p_interval)
+
+        # bynweekday is a tuple of (weekday, week_in_month) tuples
+        print "bynweekday is %s" % (bynweekday,)
+        for rule_pair in bynweekday:
+            print "rule_pair is %s" % (rule_pair,)
+
+            # Get the ordinal.
+            p_ordinal = rrule2text.ORDINAL[rule_pair[1]]
+            text_description.append(p_ordinal)
+
+            #  Get the weekday name
+            p_weekday = rrule2text.WEEKDAY_LONG[rule_pair[0] % 7][1]
+            text_description.append(p_weekday)
+            
+            # tack on "and interval" for the next item in the list
+            text_description.append(["and", p_interval])
+
+        # remove the last "and interval" because it's hanging off the end
+        # TODO improve this
+        
+        if count != 0:
+            text_description.extend([int2word(count), "times"])
+        elif until:
+            text_description.extend(["until", until])
+            
+        text_description = text_description[:-2]
+        return text_description
 
 
 class rrule2textTests(unittest.TestCase):
@@ -53,8 +119,9 @@ class rrule2textTests(unittest.TestCase):
 		pass
 		
 	def test_monthly(self):
-	    rr = rrule(dr.MONTHLY, byweekday=relativedelta.FR(3), dtstart=datetime(2011, 8, 15), count=10)
-	    self.assertEqual(rr.printrule(), "third Friday")
+	    rr = rrule2text(MONTHLY, byweekday=FR(3), dtstart=datetime(2011, 8, 15), count=10)
+	    print "text is %s" % rr.rrule2text()
+	    self.assertEqual(rr.rrule2text(), ["each", "third", "Friday", "ten", "times"])
 
 
 if __name__ == '__main__':
